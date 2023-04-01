b66e84c233b983157146f25de209c4a1d153c875
Ricardo Tomasi <ricardobeat@gmail.com>
2023-04-01 23:27:40 +0200

    pagination

--- src/app/components/InvoicesList/index.tsx
+++ src/app/components/InvoicesList/index.tsx
@@ -1,10 +1,11 @@
-import { useCallback } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack'
 import Table from 'react-bootstrap/Table'
 import Spinner from 'react-bootstrap/Spinner'
+import Pagination from 'react-bootstrap/Pagination'
 
 import { useApi } from 'api'
 import { useHistory } from 'react-router-dom'
@@ -17,14 +18,25 @@ import {
 } from 'app/lib/formatting'
 import Container from 'react-bootstrap/esm/Container'
 
+const PAGE_SIZE = 15
+
 const InvoicesList = (): React.ReactElement => {
   const api = useApi()
   const history = useHistory()
 
+  const totalPages = useRef(1)
+  const totalEntries = useRef(0)
+  const [currentPage, setCurrentPage] = useState(1)
+
   const fetchInvoices = useCallback(async () => {
-    const { data } = await api.getInvoices()
+    const { data } = await api.getInvoices({
+      page: currentPage,
+      per_page: PAGE_SIZE,
+    })
+    totalPages.current = data.pagination.total_pages
+    totalEntries.current = data.pagination.total_entries
     return data.invoices
-  }, [api])
+  }, [api, totalPages, currentPage])
 
   const { data: invoices, loading } = useFetch(fetchInvoices, [])
 
@@ -75,15 +87,47 @@ const InvoicesList = (): React.ReactElement => {
             ))}
           </tbody>
         </Table>
+
         {loading && (
           <Alert style={{ backgroundColor: '#fafafa' }}>
             <Spinner size="sm" />
             <small className="ms-3">loading invoices...</small>
           </Alert>
         )}
+
+        {!loading && (
+          <Stack direction="horizontal" className="justify-content-between">
+            <Pagination>
+              {[...Array(totalPages.current)].map((_, i) => {
+                const n = i + 1
+                return (
+                  <Pagination.Item
+                    key={n}
+                    active={n === currentPage}
+                    onClick={() => setCurrentPage(n)}
+                  >
+                    {n}
+                  </Pagination.Item>
+                )
+              })}
+            </Pagination>
+            <p>
+              Results {paginationRangeStart(currentPage)}-
+              {paginationRangeEnd(currentPage)} of {totalEntries.current}
+            </p>
+          </Stack>
+        )}
       </Stack>
     </Container>
   )
+}
+
+function paginationRangeStart(currentPage: number) {
+  return PAGE_SIZE * (currentPage - 1) + 1
+}
+
+function paginationRangeEnd(currentPage: number) {
+  return PAGE_SIZE * currentPage
 }
 
 export default InvoicesList
